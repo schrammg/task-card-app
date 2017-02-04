ce4da08a71e00d9c3c7355d4cbeabcf813c547d9
Günter <schramm.guenter@gmail.com>
2017-02-04 17:37:47 +0100

    cards

--- app/card/card.component.js
+++ app/card/card.component.js
@@ -5,23 +5,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
-var task_1 = require("./model/task");
-var AppComponent = (function () {
-    function AppComponent() {
-        this.tasks = [
-            new task_1.Task("Buy a monkey", false),
-            new task_1.Task("Walk the turtle", false)
-        ];
+var task_1 = require("../model/task");
+var CardComponent = (function () {
+    function CardComponent() {
     }
-    return AppComponent;
+    return CardComponent;
 }());
-AppComponent = __decorate([
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", task_1.Task)
+], CardComponent.prototype, "task", void 0);
+CardComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
-        selector: 'my-app',
-        templateUrl: 'app.component.html'
+        selector: 'app-card',
+        templateUrl: 'card.component.html',
+        styleUrls: ['card.component.css']
     })
-], AppComponent);
-exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.component.js.map
+], CardComponent);
+exports.CardComponent = CardComponent;
+//# sourceMappingURL=card.component.js.map
